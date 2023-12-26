9aa901559adace58509cb84cc0fb823e0e9926fa
Saksham Aggarwal <sakshamiitroorkee3@gmail.com>
2023-12-27 02:12:58 +0530

    samesite

--- Server/controllers/user.logout.controller.js
+++ Server/controllers/user.logout.controller.js
@@ -3,7 +3,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 const cookieOptions = {
   httpOnly: true,
   secure: true,
-  SameSite: 'none',
+  samesite: 'none',
 };
 const LogoutUser = async (req, res, next) => {
   const user_id = req._id;
