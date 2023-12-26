1f7c0e289f03797e31758dde6af924b075eeff9e
Saksham Aggarwal <sakshamiitroorkee3@gmail.com>
2023-12-27 02:40:16 +0530

    change directory

--- Server/utils/multer.js
+++ Server/utils/multer.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/IMG_WINTER_ASSIGNMENT/Server/public/temp");
+    cb(null, "/Server/public/temp");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
