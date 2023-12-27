2ea08eeed39153bf139683b3c1f552564979f729
Saksham Aggarwal <sakshamiitroorkee3@gmail.com>
2023-12-28 02:46:29 +0530

    updated the job details controller.

--- Server/controllers/getjobdetaisl.js
+++ Server/controllers/getjobdetaisl.js
@@ -2,14 +2,18 @@ import { Jobs } from "../models/jobs.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import { User } from "../models/user.model.js";
 
 const getJobDetails = asyncHandler( async (req, res, next)=>{
     const { jobId } = req.body;
     if(!jobId) throw new ApiError(500, "can not get JobId");
     const Job = await Jobs.findById(jobId);
+    const email = Job?.email; 
+    const jobHoster = await User.findOne({email}).select("-_id -_refreshToken -_password -_updatedAt");
+    if(!jobHoster) throw new ApiError(400, "Job Hoster not found");
+
     if(!Job) throw new ApiError(510, "Job not found");
-    res.status(200).json(new ApiResponse(200,Job,"Job fetched Successfully"));
+    res.status(200).json(new ApiResponse(200,[Job, jobHoster],"Job fetched Successfully"));
 })
 
 export { getJobDetails };
