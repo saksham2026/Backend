9d787fc9e0b95241dcd8c61c975166989887af82
Saksham Aggarwal <sakshamiitroorkee3@gmail.com>
2023-12-27 14:27:07 +0530

    changed the directory in multer.

--- Server/utils/multer.js
+++ Server/utils/multer.js
@@ -1,9 +1,7 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "/Server/public/temp");
-  },
+  destination: require.main?.path + "/" +" public/images/ ",
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, file.fieldname + "-" + uniqueSuffix);
