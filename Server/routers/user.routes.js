f5bc941f05c85e9a979a7037bc964f5f97d49738
saksham <sakshamiitroorkee3@gmail.com>
2024-01-14 19:10:45 +0530

    filter freelancers route was missing.

--- Server/routers/user.routes.js
+++ Server/routers/user.routes.js
@@ -18,6 +18,7 @@ import { getJobsByUsername } from "../controllers/getjobsbyusername.js";
 import { getRole } from "../controllers/user.getrole.js";
 import { deleteJob } from "../controllers/deletejob.controller.js";
 import { getFilteredJobs } from "../controllers/getfilteredjob.js";
+import { getFilterFreelancers } from "../controllers/fiterfreelancers.controller.js";
 import { acceptProposal, getMyJobs, getPropoposals, jobApply, rejectProposal } from "../controllers/job.controllers.js";
 const Router = express.Router();
  
@@ -59,6 +60,7 @@ Router.route("/getalljobs").post(jwtAuth, getAllJobs);
 Router.route("/getrole").post(jwtAuth,getRole);
 Router.route("/deletejob").post(jwtAuth, deleteJob);
 Router.route("/filterjobs").post(jwtAuth, getFilteredJobs);
+Router.route("/fiterfreelancers").post(jwtAuth, getFilterFreelancers);
 Router.route("/jobapply").post(jwtAuth, jobApply);
 Router.route("/getproposals").post(jwtAuth, getPropoposals);
 Router.route("/accepted").post(jwtAuth, acceptProposal);
